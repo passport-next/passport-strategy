530fb3dea6bce7a828ce4e97d679c771bc345abf
Brett Zamir <brettz9@yahoo.com>
2026-08-11 19:28:55 -0700

    feat: add `EnhancedStrategy` class (for typings if nothing else)

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import Strategy from '../lib/index.js';
+import { Strategy, EnhancedStrategy } from '../lib/index.js';
 
 describe('Strategy', function () {
   const strategy = new Strategy();
@@ -8,5 +8,39 @@ describe('Strategy', function () {
     expect(function () {
       strategy.authenticate({}, {});
     }).to.throw(Error, 'Strategy#authenticate must be overridden by subclass');
+  });
+});
+
+describe('EnhancedStrategy', function () {
+  const strategy = new EnhancedStrategy();
+
+  it('success should throw error', function () {
+    expect(function () {
+      strategy.success({}, {});
+    }).to.throw(Error, 'EnhancedStrategy#success must be overridden by subclass');
+  });
+
+  it('fail should throw error', function () {
+    expect(function () {
+      strategy.fail('challenge', 200);
+    }).to.throw(Error, 'EnhancedStrategy#fail must be overridden by subclass');
+  });
+
+  it('redirect should throw error', function () {
+    expect(function () {
+      strategy.redirect('https://example.com', 200);
+    }).to.throw(Error, 'EnhancedStrategy#redirect must be overridden by subclass');
+  });
+
+  it('pass should throw error', function () {
+    expect(function () {
+      strategy.pass();
+    }).to.throw(Error, 'EnhancedStrategy#pass must be overridden by subclass');
+  });
+
+  it('error should throw error', function () {
+    expect(function () {
+      strategy.error(new Error('error'));
+    }).to.throw(Error, 'EnhancedStrategy#error must be overridden by subclass');
   });
 });
